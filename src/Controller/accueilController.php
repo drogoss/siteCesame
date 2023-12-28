6e0fcda65e35bc8beb27ddf3de986e86e2d11831
lois <lois.hamm03@gmail.com>
2023-12-28 16:11:54 +0100

    Phase 1: create project

---
+++ src/Controller/accueilController.php
@@ -0,0 +1,21 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Description of accueilController
+ *
+ * @author loish
+ */
+class accueilController extends AbstractController {
+    /**
+     * @Route("/", name="accueil")
+     * @return Response
+     */
+    public function index(): Response{
+        return $this->render("pages/accueil.html.twig");
+    }
+}
